c264771823baa240a8e4f764c8b490fcf027830e
Sara Bocciardi <sara@fandor.com>
2015-09-26 16:58:51 -0700

    Addding lesson 4.

---
+++ lesson_4.rb
@@ -0,0 +1,25 @@
+# Now that you know how to interact with the user, let's learn how to interact with hashes and arrays.
+
+# ARRAYS
+# Arrays can be instantiated in a few different ways, but the most common is to create an empty array.
+# For example: foo = []
+# Items can be added to an array using .push, or removed from an array using .pop.
+# However, a more common Ruby syntax for adding an item to an array is the shovel: <<
+# For example: foo << 3
+# You can, of course, always start with items in your array, which should be separated by a comma.
+# For example: foo = [1, 2, 3]
+
+# Write a basic program that asks three different questions and stores the answers in an array.
+
+
+# HASHES
+# Similar to Arrays, Hashes are often instantiated by creating an empty hash.
+# For example: foo = {}
+# To add an item to a Hash, you need to specify both the key and value for the item.
+# For example: foo[:bar] = 'bash'
+# To start with items in a Hash, you need to specify key value pairs separated by commas.
+# For example: foo = {bar: 1, bash: 2, buzz: 3}
+# In older Ruby programs, you might see the hashrocket syntax. But the above is a more modern syntax.
+# Avoid instantiating your Hashes like this: foo = {:bar => 1, :bash => 2, :buzz => 3}
+
+# Write a basic program that asks three different questions and stores the answers in a hash.
