32d7169b0ff7738ed3323a741c1d7d6763929d64
Sara Bocciardi <sara@fandor.com>
2015-09-26 16:50:17 -0700

    Adding lesson 3.

---
+++ lesson_3.rb
@@ -0,0 +1,10 @@
+# In lesson 2, you learned all about data types.
+# You also need to know how to interact with a user.
+# 'puts' is a method that will print any string to the user, followed by a newline.
+# 'gets.chomp' is a method that will accept a string from the user.
+
+# Write a basic program that asks a user for his/her name and then greets them.
+
+
+# Write a basic program that asks a user for two numbers and adds them.
+# Hint: 'gets.chomp' always returns a string. Call .to_i on the user inputs to convert them to integers.
