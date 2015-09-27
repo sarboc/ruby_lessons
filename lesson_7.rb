c91f9bbd36a7ef81cd76b347cc72cbed601dc713
Sara Bocciardi <sara@fandor.com>
2015-09-26 17:10:04 -0700

    Stubbing lessons 7 and 8.

---
+++ lesson_7.rb
@@ -0,0 +1,1 @@
+# Methods, and how to pass them parameters
---
+++ lesson_8.rb
@@ -0,0 +1,7 @@
+# Let's return to an earlier lesson, but make it better.
+# For each of the follow tasks, make sure to keep your code as DRY as possible (don't repeat yourself).
+# Use at least one method per task.
+
+# Write a basic program that asks three different questions and stores the answers in an array.
+
+# Write a basic program that asks three different questions and stores the answers in a hash.
